0a1f3943f5b81c5d904e7d65e896d39af16a20a7
hotio <26902309+hotio@users.noreply.github.com>
2020-09-13 14:55:32 +0200

    fix test

--- update.sh
+++ update.sh
@@ -14,7 +14,7 @@ elif [[ ${1} == "tests" ]]; then
     docker run --rm --entrypoint="" "${2}" apk -vv info | sort
     echo "Check if app works..."
     echo "borg --version && export BORG_PASSPHRASE=testpassphrase && mkdir /tmp/borgtest && borg init --encryption=repokey-blake2 /tmp/borgtest && borg create --verbose --filter AME --list --stats --show-rc --compression lz4 --exclude-caches /tmp/borgtest::'{hostname}-{now}' /var/log/ && borg prune --list --prefix '{hostname}-' --show-rc --keep-daily 7 --keep-weekly 4 --keep-monthly 6 /tmp/borgtest" > borgtest.sh
-    docker run --rm --entrypoint="" -v "${2}" sh "${GITHUB_WORKSPACE}/borgtest.sh"
+    docker run --rm --entrypoint="" -v "${GITHUB_WORKSPACE}":"${GITHUB_WORKSPACE}" "${2}" sh "${GITHUB_WORKSPACE}/borgtest.sh"
 else
     version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/borgbackup/borg/releases/latest" | jq -r .tag_name | sed s/v//g)
     [[ -z ${version} ]] && exit 1
