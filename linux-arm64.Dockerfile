66ff072fefbd64ed55db4eb2ff627b926501771d
hotio <26902309+hotio@users.noreply.github.com>
2020-09-13 14:49:35 +0200

    rework workflow

--- linux-arm64.Dockerfile
+++ linux-arm64.Dockerfile
@@ -2,25 +2,10 @@ FROM alpine@sha256:3b3f647d2d99cac772ed64c4791e5d9b750dd5fe0b25db653ec4976f7b728
 LABEL maintainer="hotio"
 ENTRYPOINT ["borg"]
 
-ARG BORG_VERSION
+ARG VERSION
 
 RUN apk add --no-cache libacl libattr fuse py3-pyzmq py3-setuptools python3 && \
     apk add --no-cache --virtual=build-dependencies acl-dev attr-dev fuse-dev py3-pip python3-dev openssl-dev build-base linux-headers && \
     apk add --no-cache --repository http://dl-cdn.alpinelinux.org/alpine/edge/testing py3-pkgconfig && \
-    pip3 install --no-cache-dir --upgrade borgbackup[fuse]==${BORG_VERSION} && \
+    pip3 install --no-cache-dir --upgrade borgbackup[fuse]==${VERSION} && \
     apk del --purge build-dependencies py3-pkgconfig
-
-ARG LABEL_CREATED
-LABEL org.opencontainers.image.created=$LABEL_CREATED
-ARG LABEL_TITLE
-LABEL org.opencontainers.image.title=$LABEL_TITLE
-ARG LABEL_REVISION
-LABEL org.opencontainers.image.revision=$LABEL_REVISION
-ARG LABEL_SOURCE
-LABEL org.opencontainers.image.source=$LABEL_SOURCE
-ARG LABEL_VENDOR
-LABEL org.opencontainers.image.vendor=$LABEL_VENDOR
-ARG LABEL_URL
-LABEL org.opencontainers.image.url=$LABEL_URL
-ARG LABEL_VERSION
-LABEL org.opencontainers.image.version=$LABEL_VERSION
--- update.sh
+++ update.sh
@@ -9,10 +9,15 @@ if [[ ${1} == "checkdigests" ]]; then
     digest=$(echo "${manifest}" | jq -r '.manifests[] | select (.platform.architecture == "amd64" and .platform.os == "linux").digest') && sed -i "s#FROM ${image}@.*\$#FROM ${image}@${digest}#g" ./linux-amd64.Dockerfile  && echo "${digest}"
     digest=$(echo "${manifest}" | jq -r '.manifests[] | select (.platform.architecture == "arm" and .platform.os == "linux").digest')   && sed -i "s#FROM ${image}@.*\$#FROM ${image}@${digest}#g" ./linux-arm-v7.Dockerfile && echo "${digest}"
     digest=$(echo "${manifest}" | jq -r '.manifests[] | select (.platform.architecture == "arm64" and .platform.os == "linux").digest') && sed -i "s#FROM ${image}@.*\$#FROM ${image}@${digest}#g" ./linux-arm64.Dockerfile  && echo "${digest}"
+elif [[ ${1} == "tests" ]]; then
+    echo "List installed packages..."
+    docker run --rm --entrypoint="" "${2}" apk -vv info | sort
+    echo "Check if app works..."
+    echo "borg --version && export BORG_PASSPHRASE=testpassphrase && mkdir /tmp/borgtest && borg init --encryption=repokey-blake2 /tmp/borgtest && borg create --verbose --filter AME --list --stats --show-rc --compression lz4 --exclude-caches /tmp/borgtest::'{hostname}-{now}' /var/log/ && borg prune --list --prefix '{hostname}-' --show-rc --keep-daily 7 --keep-weekly 4 --keep-monthly 6 /tmp/borgtest" > borgtest.sh
+    docker run --rm --entrypoint="" -v "${2}" sh "${GITHUB_WORKSPACE}/borgtest.sh"
 else
     version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/borgbackup/borg/releases/latest" | jq -r .tag_name | sed s/v//g)
     [[ -z ${version} ]] && exit 1
-    sed -i "s/{BORG_VERSION=[^}]*}/{BORG_VERSION=${version}}/g" .github/workflows/build.yml
-    version="${version}"
+    echo "VERSION=${version}" > VERSION
     echo "##[set-output name=version;]${version}"
 fi
